9c76677ffb83df228a7b2d22c4cd57fa8ec0b8b8
Sailorspaceman <jonathonjohnsonreef@gmail.com>
2024-06-30 21:12:49 -0600

    done

--- bank_app.hpp
+++ bank_app.hpp
@@ -1,17 +1,34 @@
+// jonathon johnson
+// 6/12/24
+//bank app
+
+
+#ifndef BANK_APP_HPP
+#define BANK_APP_HPP
+
+
 #include <vector>
 
 class bankApp{
     public:
+        bankApp(double q_intdeposit, double q_monthlydeposit, double q_yearlyinterest, int q_year);
         // get all inputs from user store in class attributes
+        std::tuple<double, double, double, int> getInput();
         void getInput();
 
         // take a year and interest rate and return the result for 1 year
-        float calculateInterest(int year, float interest);
+        float calculateInterest();
 
         // this one takes all years, runs a loop, and calls calculateInterest
         // on each year
         std::vector<float> aggResult();
     private:
-        int year_input = 0;
-        float interest_input = 0.0;
+       double w_intdeposit;
+       double w_intstart;
+       double w_monthlydeposit;
+       double w_yearlyinterest;
+       int w_year;
+
 };
+
+#endif
